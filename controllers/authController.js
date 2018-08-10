68124b8b1ae95d43eed0dbdd89034b697c89c01e
Justin Evangelista <justinevangelista@Justins-MacBook-Pro.local>
2018-08-10 10:10:15 -0500

    presentation commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.post('/login', async (req, res) => {
+router.post('/register', async (req, res) => {
   console.log(req.session, ' this is session')
 
 
@@ -26,6 +26,6 @@ router.post('/login', async (req, res) => {
   }
 });
 
-
+// make a login route
 
 module.exports = router;
